102e8a9808680f40f04b6bafae815c8250e0db10
ghanshyamjarvis <ghanshyamjarvis@gmail.com>
2019-11-30 18:31:31 +0530

    Anonymous Function

---
+++ AnonymousFunction.js
@@ -0,0 +1,29 @@
+
+
+
+/*
+
+var point = function (f1)
+{
+  return "ghanshyam"
+}
+console.log(point())
+
+function rr()
+{
+  var name;
+  name = point()
+  console.log(name)
+}
+rr()
+*/
+
+var first = function gs(s,y) {return s*y}
+
+function last()
+{
+  var h;
+  h= first(10,20)
+  console.log(h)
+}
+last()
