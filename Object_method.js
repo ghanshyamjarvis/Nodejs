efc8ae25d778d54a8a88192e2940fda0f94812f1
ghanshyamjarvis <ghanshyamjarvis@gmail.com>
2019-12-02 18:02:27 +0530

    method object

---
+++ Object_method.js
@@ -0,0 +1,17 @@
+
+
+
+var person ={ Fname : "SHYAM",
+              Lname : "CHAUHAN",
+              fname : function () {return "SHYAM CHAUHAN"}
+
+  };
+console.log(person.Fname);
+console.log(person.Lname);
+console.log(person.fname());
+
+
+var h ="Hello";
+var b = {h};
+
+console.log(b.h);
