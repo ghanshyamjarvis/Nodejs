1ea360f305214837a679c2334c707cdf79967b01
ghanshyamjarvis <ghanshyamjarvis@gmail.com>
2019-12-02 17:54:55 +0530

    Binary Octal Hexa

---
+++ Binary_and_Octal_Literals.js
@@ -0,0 +1,22 @@
+
+console.log(0b001)
+
+console.log(0b010)
+
+console.log(0b011)
+
+console.log(0b100)
+
+
+//Example − Octal Representation
+
+console.log(0x010)
+
+console.log(0x100)
+
+
+
+
+console.log(0x010)
+
+console.log(0x100)
