7aa4fe64e4058a5039997d60c5aada61108b1fef
ghanshyamjarvis <ghanshyamjarvis@gmail.com>
2019-12-04 18:59:52 +0530

    Promises

--- promise.js
+++ promise.js
@@ -33,19 +33,25 @@
 
   //    The following example issues three then() calls with getSum() method.
 
-  function getSum(n1, n2) {
-    varisAnyNegative = function() {
-      return n1 < 0 || n2 < 0;
-    }
+  function getSum(n1, n2)
+        {
+          varisAnyNegative = function()
+            {
+              return n1 < 0 || n2 < 0;
+            }
+
     var promise = new Promise(function(resolve, reject) {
-      if (isAnyNegative()) {
-        reject(Error("Negatives not supported"));
-      }
-      resolve(n1 + n2);
-    });
-    return promise;
-  }
-  getSum(5, 6)
+
+      if (isAnyNegative())
+        {
+          reject(Error("Negatives not supported"));
+        }
+          resolve(n1 + n2);
+      });
+      return promise;
+    }
+
+    getSum(5, 6)
     .then(function(result) {
         console.log(result);
         returngetSum(10, 20);
