888612c8e5ae121ee7c557887bb73704e0092c3d
ghanshyamjarvis <ghanshyamjarvis@gmail.com>
2019-12-06 18:23:08 +0530

    Debugger

---
+++ Debugger.js
@@ -0,0 +1,17 @@
+x = 5;
+setTimeout(() => {
+  debugger;
+  console.log('world');
+}, 1000);
+console.log('hello');
+
+
+
+
+y =10
+setTimeout( ()=> {
+    debugger
+    console.log("Hey")
+  },2000)
+
+console.log("yes")
